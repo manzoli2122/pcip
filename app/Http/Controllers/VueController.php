9ea96d20832ff38a120405182cf1553e6620e8c6
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-07-06 21:26:09 -0300

    melhorias no controller padrao

--- app/Http/Controllers/VueController.php
+++ app/Http/Controllers/VueController.php
@@ -6,7 +6,6 @@ use Illuminate\Http\Request;
 
 use View;
 
-// use DB ;
 
 class VueController extends Controller
 {
@@ -18,15 +17,6 @@ class VueController extends Controller
     
     protected $view   ;
     
-
-    // public function __construct(Disciplina $disciplina){        
-    //     $this->model = $disciplina;         
-    //     $this->middleware('auth');                                 
-    // }
-
-
-
-
 
 
     public function index(Request $request){          
@@ -46,14 +36,12 @@ class VueController extends Controller
     {
         $this->validate( $request , $this->model->rules() );         
         
-        $this->model->create( $request->all() );
+        if(!$insert  = $this->model->create( $request->all() ) ){
+            return response()->json( 'Cadastro não realizado.' , 500);
+        }
 
         return response()->json( 'Cadastro realizado com sucesso' , 200);
-        // Disciplina::forceCreate([
-        //     'nome' => request('nome'),
-        //     'descricao' => request('descricao')
-        // ]); 
-        // return ['message' => 'Disciplina Created!'];
+        
     }
 
 
@@ -75,16 +63,17 @@ class VueController extends Controller
     public function update(Request $request ,  $id )
     {        
         $this->validate( $request  , $this->model->rules() ); 
+        
         if( !$model = $this->model->find($id) ){                 
-            return response()->json( '' , 404);
+            return response()->json( 'Item Não encontrado.' , 404);
         }  
+
         if( !$update = $model->update($request->all()) ){
-            return response()->json(['error' => 'not_update'], 500);
+            return response()->json('Item não atualizado.', 500);
         }
 
         return response()->json( 'Atualização realizada com sucesso' , 200);
-        // return ['message' => 'Disciplina Updated!'];        
-
+       
     }
     
 
@@ -114,10 +103,7 @@ class VueController extends Controller
             $models = $this->model->getDatatable();
             $result = \Yajra\DataTables\DataTables::of($models)
                 ->addColumn('action', function($linha) {
-                    return '<button data-id="'.$linha->id.'" type="button" class="btn btn-danger  btn-xs btn-datatable" btn-excluir title="Excluir">                               <i class="fa fa-times"> </i> </button> '
-                                                   
-                         . '<a href="#/edit/'.$linha->id.'" class="btn btn-success btn-datatable" title="Editar" style="margin-left: 10px;"><i class="fa fa-pencil"></i></a>'
-
+                    return  '<a href="#/edit/'.$linha->id.'" class="btn btn-success btn-datatable" title="Editar" style="margin-left: 10px;"><i class="fa fa-pencil"></i></a>'
                          . '<a href="#/show/'.$linha->id.'" class="btn btn-primary btn-datatable" title="Visualizar" style="margin-left: 10px;"><i class="fa fa-search"></i></a>'
                         ;
                 })->make(true);
@@ -143,15 +129,15 @@ class VueController extends Controller
         try
         {                      
             if( !$model = $this->model->find($id) ){     
-                return response()->json(['erro' => true , 'msg' => 'Aconteceu um Erro interno.'  , 'data' => null ], 200);  
+                return response()->json('Item não encontrado.' , 404 );  
             }
             if( !$delete = $model->delete() ){
-                return response()->json(['erro' => true , 'msg' => 'Aconteceu um Erro interno.'  , 'data' => null ], 200);
+                return response()->json('Aconteceu um Erro interno.' , 500);
             }
             return response()->json(['erro' => false , 'msg' => $this->name . ' alterado no sistema' , 'data' =>  $delete ], 200 );            
         }         
         catch (Exception $e) {
-                return response()->json(['erro' => true , 'msg' => 'Aconteceu um Erro interno.'  , 'data' => null ], 200);
+                return response()->json( 'Aconteceu um Erro interno.', 500);
         }
     }
  
@@ -167,12 +153,12 @@ class VueController extends Controller
     {
         try {
             if( !$model = $this->model->find($id) ){       
-                return response()->json(['erro' => true , 'msg' => ''  , 'data' => null ], 200);    
+                return response()->json('Item não encontrado.', 404 );    
             }                   
             return response()->json( $model , 200);
         }         
         catch(Exception $e) {           
-            return response()->json(['erro' => true , 'msg' => ''  , 'data' => null ], 200);    
+            return response()->json( 'Erro interno', 500);    
         }
     }
 
